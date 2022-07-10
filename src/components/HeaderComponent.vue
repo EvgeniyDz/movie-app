a2ea70a8dc9895a4f38bb62f298e88ff9bc2e56a
Євгеній Джулай <evgenijdzulaj@MacBook-Pro-Evgenij.local>
2022-07-10 18:56:06 +0300

    Add header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+      <p>MovieApp</p>
+        <b-button squared>Home</b-button>
+        <b-button squared>Favorite</b-button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
